afc0fe6b1d7c729af15179c57ad068e363a36a5e
Krishnanshu sharma <61700585+white-panda-afk@users.noreply.github.com>
2020-08-01 18:50:08 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
 
     // PRE LOADER
     $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+      $('.preloader').fadeOut(100); // set duration in brackets    
     });
     
 
